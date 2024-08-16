b5c74ccfc420327d9e83fda63d8873dd9aa553f3
matias-ovalle-toteat <matias@toteat.com>
2024-08-15 21:37:37 -0400

    fix: remove multiple redundant calls to app.run()

--- backend/main.py
+++ backend/main.py
@@ -17,8 +17,3 @@ app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
-    app.run(debug=True)
-    app.run(debug=True)
-    app.run(debug=True)
-    app.run(debug=True)
-    app.run(debug=True)
